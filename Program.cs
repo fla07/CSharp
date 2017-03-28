a3824e5b3a181e2ede84ed63cd383ecd8b36efe4
Flavia Nunes Pino <flavitta007@gmail.com>
2017-03-28 14:20:02 -0300

    Hello World
    First steps to create a program using C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Is like that we write on console scream using c#
+            Console.WriteLine("Hello World!");
+            Console.Write("Now, I'm wrintting in the same line... ");
+            Console.Write("But using two commands... ");
+            //And is like that we get information from scream
+            Console.ReadLine();
+        }
+    }
+}
